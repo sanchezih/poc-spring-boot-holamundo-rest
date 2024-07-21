e8935c7f3e5b6b01df89dfa2fc25d101dd2798a3
Ignacio Sanchez <sanchezih@gmail.com>
2024-07-21 14:59:59 -0300

    upload app

---
+++ src/main/java/com/github/sanchezih/holamundo/controller/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.github.sanchezih.holamundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HolaMundoController {
+
+	@GetMapping(path = "/holamundo")
+	public String getHolaMundo() {
+		return "Hola mundo!";
+	}
+}
